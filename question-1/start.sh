02d82de4120b66aec0a47f498b635f617c6b9829
jonathan <jonatanshtadler@gmail.com>
2023-10-05 08:36:40 +0300

    Updated q1

--- question-1/start.sh
+++ question-1/start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo "Cleaning and setting things up, please wait."
+
+echo "Cleaning and setting things up for you."
 minikube delete &> /dev/null
+echo "Just one more moment please, I feel you I can't wait to start too!"
 minikube start &> /dev/null
-echo "Just one more moment."
+
 
 kubectl config set-context hello &> /dev/null
 kubectl config set-context world &> /dev/null
+
+
+echo "Ready to go."
--- question-1/test.sh
+++ question-1/test.sh
@@ -3,19 +3,20 @@
 counter=0
 
 if [ -e contexts.txt ]; then
-    if diff -q <(echo -e "hello\nminikube\nworld") contexts.txt; then
+    if diff -q <(echo -e "hello\nminikube\nworld") contexts.txt &> /dev/null; then
         counter=$((counter + 1))
     else
-        echo "one of the contexts is missing in contexts.txt"
+        echo "one or more of the contexts is missing in contexts.txt"
     fi
 else
     echo "file contexts.txt not found"
 fi
 
+
 if [ -e current_context_kubectl.sh ]; then
     chmod +x current_context_kubectl.sh
     echo "$(./current_context_kubectl.sh)" > temp.txt
-    if grep -q "kubectl" current_context_kubectl.sh && diff -q <(echo -e "minikube") temp.txt; then
+    if grep -q "kubectl" current_context_kubectl.sh && diff -q <(echo -e "minikube") temp.txt &> /dev/null; then
         counter=$((counter + 1))
     else
         echo "the command in current_context_kubectl.sh is not valid"
@@ -25,10 +26,18 @@ else
     echo "file current_context_kubectl.sh not found"
 fi
 
+
 if [ -e current_context_no_kubectl.sh ]; then
     chmod +x current_context_no_kubectl.sh
     ./current_context_no_kubectl.sh > temp.txt
-    if diff -q <(echo -e "minikube") temp.txt && ! grep -q "kubectl" current_context_no_kubectl.sh; then
+    line_not_starts_with_k=0
+    while IFS= read -r line; do
+        first_char="${line:0:1}"
+        if [ "$first_char" = "k" ]; then
+            line_not_starts_with_k=1
+        fi
+    done < "current_context_no_kubectl.sh"
+    if diff -q <(echo -e "minikube") temp.txt &> /dev/null && [ $line_not_starts_with_k -eq 0 ] && ! grep -q "kubectl" current_context_no_kubectl.sh; then
         counter=$((counter + 1))
     else
         echo "the command in current_context_no_kubectl.sh is not valid"
